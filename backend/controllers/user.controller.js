2ffcff336b69b12a59379742b823ab960153526e
Sagar <sagar20032610@gmail.com>
2025-08-10 18:41:22 +0530

    deployment 4

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,7 +92,17 @@ export const login = async (req, res) => {
             following: user.following,
             posts: user.posts
         }
-        return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+        // return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+        //     message: `Welcome back ${user.username}`,
+        //     success: true,
+        //     user
+        // });
+                return res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,         
+            sameSite: 'none',      
+            maxAge: 1 * 24 * 60 * 60 * 1000
+        }).json({
             message: `Welcome back ${user.username}`,
             success: true,
             user
